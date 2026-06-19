acf0d6116dfbd1a66558dd79449117ed62906ce9
agungtirtayasakusuma <agungtirtayasaakusuma@gmail.com>
2026-06-19 20:38:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
-from database import conn, cursor
+from schemas import BookingCreate
 
 app = FastAPI(
     title="Avenir Booking API"
 )
 
+# DATA SEMENTARA
+bookings = [
+    {
+        "id": 1,
+        "customer_id": 1,
+        "tour_id": 1,
+        "pax": 2,
+        "status": "Pending"
+    }
+]
 
+# HOME
 @app.get("/")
 def home():
     return {
@@ -13,15 +24,60 @@ def home():
     }
 
 
+# GET BOOKINGS
 @app.get("/bookings")
 def get_bookings():
+    return bookings
 
-    cursor.execute("""
-        SELECT *
-        FROM bookings
-        ORDER BY booking_id
-    """)
 
-    data = cursor.fetchall()
+# CREATE BOOKING
+@app.post("/booking")
+def create_booking(booking: BookingCreate):
 
-    return data
+    booking_data = booking.model_dump()
+
+    bookings.append(booking_data)
+
+    return {
+        "message": "Booking created",
+        "data": booking_data
+    }
+
+
+# UPDATE BOOKING
+@app.put("/booking/{booking_id}")
+def update_booking(booking_id: int):
+
+    for booking in bookings:
+
+        if booking["id"] == booking_id:
+
+            booking["status"] = "Paid"
+
+            return {
+                "message": "Booking updated",
+                "data": booking
+            }
+
+    return {
+        "message": "Booking not found"
+    }
+
+
+# DELETE BOOKING
+@app.delete("/booking/{booking_id}")
+def delete_booking(booking_id: int):
+
+    for booking in bookings:
+
+        if booking["id"] == booking_id:
+
+            bookings.remove(booking)
+
+            return {
+                "message": "Booking deleted"
+            }
+
+    return {
+        "message": "Booking not found"
+    }
